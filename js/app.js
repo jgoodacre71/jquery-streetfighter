9b3f8c10babcdbbac6b8c16495a90e4e277ca0a1
John Goodacre <jgoodacre71@gmail.com>
2015-06-10 13:14:13 +0100

    finished bar some messing

--- js/app.js
+++ js/app.js
@@ -2,32 +2,54 @@ $(document).ready(function() {
 
 	$('.ryu').mouseenter(function() {
     	$('.ryu-still').hide();
+    	$('.ryu-looking-cool').hide();
 		$('.ryu-ready').show();
 	})
 	.mouseleave(function()  {
 		$('.ryu-ready').hide();
+		$('.ryu-looking-cool').hide();
 		$('.ryu-still').show();
 	})
 	.mousedown(function() {
     playHadouken();
     $('.ryu-ready').hide();
+    $('.ryu-still').hide();
+    $('.ryu-looking-cool').hide();
     $('.ryu-throwing').show();
     $('.hadouken').finish().show().animate(
-    {'left': '1020px'},
-    500,
-  	function() {
-    $(this).hide();
-    $(this).css('left', '520px');
-  }
-
-    );
+    			{'left': '1020px'},
+    			500,
+  				function() {
+    			$(this).hide();
+    			$(this).css('left', '520px');
+  				});
 	})
     // animate hadouken to the right of the screen
     .mouseup(function() {
   	$('.ryu-throwing').hide();
+  	$('.ryu-looking-cool').hide();
   	$('.ryu-ready').show();
 	});
+
+    $(document).keydown(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-ready').hide();
+      $('.ryu-still').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-looking-cool').show();
+    }   
+  }).keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu-looking-cool').hide();
+      $('.ryu-ready').hide();
+       $('.ryu-throwing').hide();
+      $('.ryu-still').show();
+    }
+  });
+
 });
+
+
 
 function playHadouken () {
   	$('#hadouken-sound')[0].volume = 0.5;
